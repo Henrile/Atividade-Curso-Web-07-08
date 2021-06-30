8ccf883f3919aa58a5a856f0246ee88ade8b030f
Pedro Henrile <45532170+Henrile@users.noreply.github.com>
2021-06-29 22:45:21 -0300

    Add Projeto Final
    Pokedex

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+
+import Axios from 'axios';
+
+import pokebolaImg from './assets/pokebola.png';
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    async function getPokemons() {
+      const response = await Axios.get('https://pokeapi.co/api/v2/pokemon/');
+
+      setPokemons(response.data.results)
+    }
+
+    getPokemons();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="header">
+        <div className="logo">
+          <img src={pokebolaImg} alt="Pokebola" />
+          <h1>POKEDEX</h1>
+        </div>
       </header>
+      <main>
+        <div className="content">
+          {pokemons.map(pokemon => {
+            const numberPokemon = pokemon.url.replace('https://pokeapi.co/api/v2/pokemon/', '').replace('/', '')
+            const imgUrl = `https://pokeres.bastionbot.org/images/pokemon/${numberPokemon}.png`
+            return(
+            <div key={pokemon.name} className="card">
+              <img src={imgUrl} alt="Foto Pokemon"/>
+              <strong>{pokemon.name}</strong>
+            </div>
+          )
+          })}
+        </div>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+
